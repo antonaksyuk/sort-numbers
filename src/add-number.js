342f7de4cbf865a6a8242c9ea02c82226922a69f
antonaksyuk <antonaksyuk@gmail.com>
2025-03-19 16:50:32 +0100

    add-num update

--- src/add-number.js
+++ src/add-number.js
@@ -1,51 +1,17 @@
-// import data from './data.js';
-// debugger;
-
-// const readNumber = (id) => {
-//     const value = document.getElementById(id).value
-//     return Number(value);
-// }
-
-// const display = (id, content) => {
-//     document.getElementById(id).innerText = content;
-// }
-
-// const addNumber = () => {
-//     const newNumber = readNumber('user-input');
-//     data.numbers.push(newNumber);
-
-//     let numbersList = '';
-//     for (const num of data.numbers) {
-//         numbersList += `* ${num}\n`;
-//     }
-//     display('all-numbers', numbersList);
-// };
-
-// document.getElementById('add-it').addEventListener('click', addNumber);
-
-
 import data from './data.js';
+debugger;
 
 const readNumber = (id) => {
-    const inputElement = document.getElementById(id);
-    if (!inputElement) {
-        console.error(`Элемент с id="${id}" не найден`);
-        return null;
-    }
-    return Number(inputElement.value);
-};
+    const value = document.getElementById(id).value
+    return Number(value);
+}
 
 const display = (id, content) => {
-    const element = document.getElementById(id);
-    if (element) {
-        element.innerText = content;
-    }
-};
+    document.getElementById(id).innerText = content;
+}
 
 const addNumber = () => {
     const newNumber = readNumber('user-input');
-    if (newNumber === null) return; // Проверяем, что число считалось
-
     data.numbers.push(newNumber);
 
     let numbersList = '';
@@ -55,6 +21,4 @@ const addNumber = () => {
     display('all-numbers', numbersList);
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('add-it').addEventListener('click', addNumber);
-});
+document.getElementById('add-it').addEventListener('click', addNumber);
